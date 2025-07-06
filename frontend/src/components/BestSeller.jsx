ac2f1345e49cba34c12b99081fd0b79ce2ec9b71
0xdivi-code <hallbryghtaa@gmail.com>
2025-07-07 00:15:40 +0100

    first-update

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -18,7 +18,7 @@ const BestSeller = () => {
       <div className='text-center text-3xl py-8'>
         <Title text1={'BEST'} text2={'SELLERS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+        Decorative Figurines, Ornaments, Phones and Accessories, Mirrors, Humidifier, Diffusers, Flowers, Artificial Plants, Wall Canvass, Art Work Frames, Window Blinds, Custom Gift Items.
         </p>
       </div>
 
